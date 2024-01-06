a295d126d36acddf6c19be0268b0463bec15f32b
km-07 <71545431+km-07@users.noreply.github.com>
2024-01-06 11:45:16 +0530

    Update about.py

--- about.py
+++ about.py
@@ -7,7 +7,7 @@ def app():
     st.balloons()
     st.title('Contact Us')
     st.markdown('''### Name:
-    Tanmay Jain''')
+    Khushal Mehrotra''')
     st.markdown('''### Email:
-    tanmayjain245@gmail.com''')
+    kmehrotra07@gmail.com''')
 #     st.markdown('''### LinkedIn: [KhushalMehrotra](https://www.linkedin.com/in/khushal-mehrotra-5276091a9/)''')
